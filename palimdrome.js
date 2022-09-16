43b989dadeba009ba80b4d380cfaa47c9b05a54b
pawan cheke <pawancheke11@gmail.com>
2022-09-16 09:37:50 +0530

    palimdrome.js file also added

---
+++ palimdrome.js
@@ -0,0 +1,11 @@
+let str="pawan";
+let N=str.length;
+for (let i=0;i<N/2;i++){
+    if(str[i] == str[N-1-i]){
+        console.log("No");
+        break;
+    }else {
+        console.log("Yes");
+        break;
+    }
+}
